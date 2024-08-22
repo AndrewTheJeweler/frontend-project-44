3bec739038e54182faeabc34684059d6d796944d
AndrewTheJeweler <bushuevandrew96@gmail.com>
2024-08-23 00:43:20 +0300

    add dir src and cli.js

---
+++ src/cli.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+
+const greetings = () => {
+	console.log(`Welcom to the Brain Games!`);
+	const name = readlineSync.question('May I have your name? ');
+	console.log(`Hello, ${name}!`);
+
+
+
+};
+
+export default greetings;
