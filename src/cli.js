b1da576fc5d8845786c979740c4ffe5e3e835daf
AndrewTheJeweler <bushuevandrew96@gmail.com>
2024-08-23 18:02:16 +0300

    fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const greetings = () => {
-	console.log(`Welcom to the Brain Games!`);
-	const name = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${name}!`);
-
-
-
+  console.log('Welcom to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 
 export default greetings;
