80eae20694cbf1d0e5e433a596dc37092024d86d
AndrewTheJeweler <bushuevandrew96@gmail.com>
2024-08-25 01:51:44 +0300

    add new file index.js with functions

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import readlineSync from 'readline-sync';
+
+// Функция для генирации рандомного числа
+const randomNumber = (min, max) => {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  return num;
+};
+
+// Функция приветсвтия (возвращает имя пользователя)
+const greetings = () => {
+  console.log('Welcom to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
+
+// Функция создания массива из заданного количества случайных символов в диапазоне 1-100
+const array = (numberOfNumbers) => {
+  const arrayOfNumber = [];
+  for (let i = 0; i < numberOfNumbers; i += 1) {
+    arrayOfNumber.push(randomNumber(1, 100));
+  }
+  return arrayOfNumber;
+};
+
+// Функция вызова рандомного оператора из массива
+const operatorArray = ['*', '+', '-'];
+const randomOperator = () => {
+  const operatorIndex = randomNumber(0, operatorArray.length - 1);
+  return operatorArray[operatorIndex];
+};
+
+// Функция создания массива из рандомных операторов
+const arrayOperators = (numbersOfOperators) => {
+  const arrayOp = [];
+  for (let i = 0; i < numbersOfOperators; i += 1) {
+    arrayOp.push(randomOperator());
+  }
+  return arrayOp;
+};
+
+export {
+  randomNumber, greetings, arrayOperators, array as arr,
+};
