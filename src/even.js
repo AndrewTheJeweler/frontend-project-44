8aacea25e83111cabcd6655bdaae70671e30ccc9
AndrewTheJeweler <bushuevandrew96@gmail.com>
2024-08-25 01:49:41 +0300

    update even.js

--- src/even.js
+++ src/even.js
@@ -1,26 +1,17 @@
 import readlineSync from 'readline-sync';
+import { greetings, arr } from './index.js'; // Функция приветствия, функция определения массива из заданного количества случайных символов
 
-// Определяем функцию для генирации рандомного числа
-const randomNumber = (min, max) => {
-  const num = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  return num;
-};
+const array = arr(3); // Создаем массив из рандомных
 
 const evenGames = () => {
-  const name = readlineSync.question('Welcom to the Brain Games! \nMay I have your name? '); // Приветствие
-  console.log(`Hello, ${name}! 
-Answer "yes" if the number is even, otherwise answer "no".`);
-  const number1 = randomNumber(1, 100);
-  const number2 = randomNumber(1, 100); // Генирируем рандомные числа
-  const number3 = randomNumber(1, 100);
-  const array = [number1, number2, number3]; // Создаем массив из рандомных чисел
+  const name = greetings(); // Передаем в констану name имя пользователя, возвращенное функцией
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
   let count = 0; // Определяем счетчик для цикла
   let i = 0; // Определяем счетчик для индекса массива
   while (count <= 2) {
     console.log(`Question: ${array[i]}`);
-    const question1 = readlineSync.question('Your answer: ');
-    if ((array[i] % 2 === 0 && question1 === 'yes') || (array[i] % 2 !== 0 && question1 === 'no')) {
+    const answer = readlineSync.question('Your answer: ');
+    if ((array[i] % 2 === 0 && answer === 'yes') || (array[i] % 2 !== 0 && answer === 'no')) {
       console.log('Correct!');
       i += 1;
       count += 1;
@@ -28,10 +19,10 @@ Answer "yes" if the number is even, otherwise answer "no".`);
         console.log(`Congratulations, ${name}!`);
       }
     } else if (array[i] % 2 === 0) {
-      console.log(`"${question1}" is wrong answer ;(. Correct answer was 'yes'.`);
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was 'yes'.`);
       break;
     } else if (array[i] % 2 !== 0) {
-      console.log(`"${question1}" is wrong answer ;(. Correct answer was 'no'.`);
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was 'no'.`);
       break;
     }
   }
