4c6447757ceda0c6ec737d35047a327bbbae2115
AndrewTheJeweler <bushuevandrew96@gmail.com>
2024-08-27 15:34:04 +0300

    add code of brain-progression

---
+++ src/games/progression.js
@@ -0,0 +1,24 @@
+import readlineSync from 'readline-sync';
+import { greetings, arrProgression, randomNumber } from '../index.js';
+
+const progressionGame = () => {
+  const name = greetings();
+  console.log('What number is missing in the progression?');
+  for (let i = 0; i < 3; i += 1) {
+    const array = arrProgression(); // Реализуем массив из рандомной арифметической прогресии
+    const removeNumber = array.splice(randomNumber(0, 9), 1, '..'); // Удаляем из массива выше рандомное число, заменяя его на '..'
+    console.log('Question: ', array.join(' '));
+    const answer = readlineSync.question('Your answer: ');
+    if (removeNumber[0] === Number(answer)) {
+      console.log('Correct!');
+      if (i === 2) {
+        console.log(`Congratulations, ${name}!`);
+      }
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${removeNumber}' \nLet's try again ${name}!`);
+      break;
+    }
+  }
+};
+
+export default progressionGame;
