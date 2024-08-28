5625c38710d9bd7c01b59824f14ce49034e2cdd2
AndrewTheJeweler <bushuevandrew96@gmail.com>
2024-08-28 16:31:36 +0300

    Optimized the code, removed logic

--- src/games/even.js
+++ src/games/even.js
@@ -1,25 +1,7 @@
-import readlineSync from 'readline-sync';
-import { greetings, arr, isNumberEven } from '../index.js'; // Функция приветствия, функция определения массива из заданного количества случайных символов
-
-const array = arr(3); // Создаем массив из рандомных чисел
+import logicsOfGames from '../index.js';
 
 const evenGames = () => {
-  const name = greetings();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for (let i = 0; i <= 2; i += 1) {
-    console.log(`Question: ${array[i]}`);
-    const answer = readlineSync.question('Your answer: ');
-    const number = isNumberEven(array[i], answer);
-    if (answer === number) {
-      console.log('Correct!');
-      if (i === 2) {
-        console.log(`Congratulations, ${name}!`);
-      }
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${number}'. \nLet's try again, ${name}!`);
-      break;
-    }
-  }
+  logicsOfGames('even');
 };
 
 export default evenGames;
