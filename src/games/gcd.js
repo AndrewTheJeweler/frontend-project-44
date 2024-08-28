1ea4942d1e2ffc7aa8b6e9e8d989482fa2b07b08
AndrewTheJeweler <bushuevandrew96@gmail.com>
2024-08-28 16:31:49 +0300

    Optimized the code, removed logic

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,28 +1,7 @@
-import readlineSync from 'readline-sync';
-import { greetings, arr, nod } from '../index.js';
-
-const array = arr(6); // Определяем массив из случайных 6 символов (диапазон 1-100 включительно)
+import logicsOfGames from '../index.js';
 
 const gcdGames = () => {
-  const name = greetings();
-  console.log('Find the greatest common divisor of given numbers.');
-  let k = 1;
-  let o = 0;
-  for (let i = 0; i <= 2; i += 1) {
-    console.log(`Question: ${array[o]} ${array[k]}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (Number(answer) === nod(array[o], array[k])) {
-      console.log('Correct!');
-      o += 2;
-      k += 2;
-      if (i === 2) {
-        console.log(`Congratulations, ${name}!`);
-      }
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${nod(array[o], array[k])}'. \nLes't try again, ${name}!`);
-      break;
-    }
-  }
+  logicsOfGames('gcd');
 };
 
 export default gcdGames;
