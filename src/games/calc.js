3b158b83ebe9fc3a706696564f1bb530cac30291
AndrewTheJeweler <bushuevandrew96@gmail.com>
2024-08-28 16:31:17 +0300

    Optimized the code, removed logic

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,32 +1,7 @@
-import readlineSync from 'readline-sync';
-import {
-  greetings, arr, arrayOperators, operatorsChek,
-} from '../index.js';
-
-const array = arr(6);
-const arrOp = arrayOperators(3);
+import logicsOfGames from '../index.js';
 
 const calcGames = () => {
-  const name = greetings();
-  console.log('What is the result of the expression?');
-  let k = 1;
-  let o = 0;
-  for (let i = 0; i <= 2; i += 1) {
-    console.log(`Question: ${array[o]} ${arrOp[i]} ${array[k]}`);
-    const rezult = operatorsChek(array[o], arrOp[i], array[k]);
-    const answer = readlineSync.question('Your answer: ');
-    if (Number(answer) === rezult) {
-      console.log('Correct!');
-      k += 2;
-      o += 2;
-      if (i === 2) {
-        console.log(`Congratulations, ${name}!`);
-      }
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rezult}' \nLet's try again, ${name}!`);
-      break;
-    }
-  }
+  logicsOfGames('calc');
 };
 
 export default calcGames;
