5e3cb64fbb420babb0350e3f6c4a5fec4130390c
AndrewTheJeweler <bushuevandrew96@gmail.com>
2024-08-28 13:53:08 +0300

    update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,7 +7,7 @@ const progressionGame = () => {
   for (let i = 0; i < 3; i += 1) {
     const array = arrProgression(); // Реализуем массив из рандомной арифметической прогресии
     const removeNumber = array.splice(randomNumber(0, 9), 1, '..'); // Удаляем из массива выше рандомное число, заменяя его на '..'
-    console.log('Question: ', array.join(' '));
+    console.log(`Question: ${array.join(' ')}`);
     const answer = readlineSync.question('Your answer: ');
     if (removeNumber[0] === Number(answer)) {
       console.log('Correct!');
@@ -15,7 +15,7 @@ const progressionGame = () => {
         console.log(`Congratulations, ${name}!`);
       }
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${removeNumber}' \nLet's try again ${name}!`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${removeNumber}' \nLet's try again, ${name}!`);
       break;
     }
   }
