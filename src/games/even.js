2f9f009b7e9e3339b8cde7b3ea0107659f776b65
AndrewTheJeweler <bushuevandrew96@gmail.com>
2024-08-28 13:52:33 +0300

    update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,28 +1,22 @@
 import readlineSync from 'readline-sync';
-import { greetings, arr } from '../index.js'; // Функция приветствия, функция определения массива из заданного количества случайных символов
+import { greetings, arr, isNumberEven } from '../index.js'; // Функция приветствия, функция определения массива из заданного количества случайных символов
 
 const array = arr(3); // Создаем массив из рандомных чисел
 
 const evenGames = () => {
-  const name = greetings(); // Передаем в констану name имя пользователя, возвращенное функцией
+  const name = greetings();
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let count = 0; // Определяем счетчик для цикла
-  let i = 0; // Определяем счетчик для индекса массива
-  while (count <= 2) {
+  for (let i = 0; i <= 2; i += 1) {
     console.log(`Question: ${array[i]}`);
     const answer = readlineSync.question('Your answer: ');
-    if ((array[i] % 2 === 0 && answer === 'yes') || (array[i] % 2 !== 0 && answer === 'no')) {
+    const number = isNumberEven(array[i], answer);
+    if (answer === number) {
       console.log('Correct!');
-      i += 1;
-      count += 1;
-      if (count === 3) {
+      if (i === 2) {
         console.log(`Congratulations, ${name}!`);
       }
-    } else if (array[i] % 2 === 0) {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, ${name}!`);
-      break;
-    } else if (array[i] % 2 !== 0) {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${number}'. \nLet's try again, ${name}!`);
       break;
     }
   }
