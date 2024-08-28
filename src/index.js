303a197a5b0a9c228fa5594c9a116ee1b782b447
AndrewTheJeweler <bushuevandrew96@gmail.com>
2024-08-28 16:30:19 +0300

    add logics of all games

--- src/index.js
+++ src/index.js
@@ -15,15 +15,6 @@ const greetings = () => {
   return name;
 };
 
-// Функция создания массива из заданного количества случайных символов в диапазоне 1-100
-const array = (numberOfNumbers) => {
-  const arrayOfNumber = [];
-  for (let i = 0; i < numberOfNumbers; i += 1) {
-    arrayOfNumber.push(randomNumber(1, 100));
-  }
-  return arrayOfNumber;
-};
-
 // Функция вызова рандомного оператора из массива
 const operatorArray = ['*', '+', '-'];
 const randomOperator = () => {
@@ -106,8 +97,99 @@ const operatorsChek = (num1, operator, num2) => {
   return rezult;
 };
 
-export {
-  randomNumber, greetings, arrayOperators, array as arr, nod, arrProgression, primeNumber,
+// Логика всех игр
+const logicsOfGames = (type = 'progression') => {
+  const name = greetings(); // Приветствие, запоминаем имя
+  if (type === 'progression') { // Проферяем тип игры - рандомная прогрессия
+    console.log('What number is missing in the progression?');
+    for (let i = 0; i <= 2; i += 1) {
+      const array1 = arrProgression(); // Реализуем массив из рандомной арифметической прогресии
+      const rezult = array1.splice(randomNumber(0, 9), 1, '..'); // Удаляем из массива выше рандомное число, заменяя его на '..'
+      const condition = array1.join(' '); // Изменяем тип на строку
+      console.log(`Question: ${condition}`);
+      const answer = readlineSync.question('Your answer: ');
+      if (rezult[0] === Number(answer)) {
+        console.log('Correct!');
+        if (i === 2) {
+          console.log(`Congratulations, ${name}!`);
+        }
+      } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rezult}' \nLet's try again, ${name}!`);
+        break;
+      }
+    }
+  } else if (type === 'prime') { // Проверка типа - является ли число простым?
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+    for (let i = 0; i <= 2; i += 1) {
+      const num = randomNumber(2, 101);
+      console.log(`Question: ${num}`);
+      const rezult = primeNumber(num); // Проверка числа (является ли оно простым? yes/no)
+      const answer = readlineSync.question('Your answer: ');
+      if (answer === rezult) {
+        console.log('Correct!');
+        if (i === 2) {
+          console.log(`Congratulations, ${name}!`);
+        }
+      } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rezult}' \nLet's try again, ${name}!`);
+        break;
+      }
+    }
+  } else if (type === 'gcd') { // Проверка типа - Наибольший общий делитель
+    console.log('Find the greatest common divisor of given numbers.');
+    for (let i = 0; i <= 2; i += 1) {
+      const num1 = randomNumber(1, 10);
+      const num2 = randomNumber(1, 10);
+      console.log(`Question: ${num1} ${num2}`);
+      const answer = readlineSync.question('Your answer: ');
+      const rezult = nod(num1, num2); // Поиск функцией максимального общего делителя
+      if (Number(answer) === rezult) {
+        console.log('Correct!');
+        if (i === 2) {
+          console.log(`Congratulations, ${name}!`);
+        }
+      } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rezult}'. \nLes't try again, ${name}!`);
+        break;
+      }
+    }
+  } else if (type === 'even') { // Проверка типа - является ли число четным
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    for (let i = 0; i <= 2; i += 1) {
+      const randomNum = randomNumber(1, 100);
+      console.log(`Question: ${randomNum}`);
+      const answer = readlineSync.question('Your answer: ');
+      const rezult = isNumberEven(randomNum, answer);
+      if (answer === rezult) {
+        console.log('Correct!');
+        if (i === 2) {
+          console.log(`Congratulations, ${name}!`);
+        }
+      } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rezult}'. \nLet's try again, ${name}!`);
+        break;
+      }
+    }
+  } else if (type === 'calc') { // Проверяем тип - калькулятор
+    console.log('What is the result of the expression?');
+    for (let i = 0; i <= 2; i += 1) {
+      const arrOp = arrayOperators(3); // Создаем массив из рандомных операторов
+      const num1 = randomNumber(1, 100);
+      const num2 = randomNumber(1, 100);
+      console.log(`Question: ${num1} ${arrOp[i]} ${num2}`);
+      const rezult = operatorsChek(num1, arrOp[i], num2);
+      const answer = readlineSync.question('Your answer: ');
+      if (Number(answer) === rezult) {
+        console.log('Correct!');
+        if (i === 2) {
+          console.log(`Congratulations, ${name}!`);
+        }
+      } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rezult}' \nLet's try again, ${name}!`);
+        break;
+      }
+    }
+  }
 };
 
-export { isNumberEven, operatorsChek };
+export default logicsOfGames;
