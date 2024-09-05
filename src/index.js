21b84f75a3ab8c82033c38e3f54a7124216faab9
AndrewTheJeweler <bushuevandrew96@gmail.com>
2024-09-05 17:31:38 +0300

    moving one-time function / update code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 import readlineSync from 'readline-sync';
-import { greetings } from './utils.js';
 
-const countRound = 3;
-const enginOfGames = (exercise, round) => {
-  const name = greetings();
-  console.log(exercise);
-  for (let i = 1; i <= countRound; i += 1) {
-    const [question, correctAnswer] = round();
-    console.log(`Question: ${question}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (correctAnswer === answer) {
-      console.log('Correct!');
-    } else if (correctAnswer !== answer) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}' \nLet's try again, ${name}!`);
-      return;
-    }
-  } console.log(`Congratulations, ${name}!`);
+const roundsCount = 3;
+
+const greetTheUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  return userName;
 };
 
-export default enginOfGames;
+const startEngin = (description, getRound) => {
+  const userName = greetTheUser();
+  console.log(description);
+  for (let i = 1; i <= roundsCount; i += 1) {
+    const [question, correctAnswer] = getRound();
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (correctAnswer !== answer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}' \nLet's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  } console.log(`Congratulations, ${userName}!`);
+};
+
+export default startEngin;
